8c05b49eb0b6508769ba60071007c90149bdd64b
arvind.pandey <arvind.pandey@ukbiobank.ac.uk>
2022-09-08 10:52:01 +0100

    Nuget package and Clock service

---
+++ HumanFriendlyClock/TimeExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanFriendlyClock
+{
+   public static class TimeExtension
+    {
+        public static string NumberToWords(this int number)
+        {
+
+            var words = "";
+            if (words != "")
+                words += "and ";
+
+            var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+            var tensMap = new[] { "Zero", "Ten", "Twenty", "Half", "Forty", "Fifty" };
+
+            if (number < 20)
+                words += unitsMap[number];
+            else
+            {
+                words += tensMap[number / 10];
+                if ((number % 10) > 0)
+                    words += " " + unitsMap[number % 10];
+            }
+
+            return words;
+        }
+    }
+}
---
+++ TalkingClockService/Controllers/TalkingClockController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using  HumanFriendlyClock;
+using Newtonsoft.Json.Linq;
+
+namespace TalkingClockService.Controllers
+{
+    [Route("api/TalkingClockController")]
+
+    [ApiController]
+    public class TalkingClockController : ControllerBase
+    {
+
+        // GET api/TalkingClockController/5:00
+
+        [HttpGet("{time}")]
+        public ActionResult<JObject> Get(string time)
+        {
+            var humanFriendlyTime = string.IsNullOrEmpty(time) || time == "\"\"" ? Time.GetHumanFriendlyTime(DateTime.Now) : Time.GetHumanFriendlyTime(Convert.ToDateTime(time));
+
+            var jsonData = @"{'Human Friendly Text':" + "'" + humanFriendlyTime.Replace("'", "\\'") + "'" + "}";
+
+            return JObject.Parse(jsonData);
+        }
+    }
+}
